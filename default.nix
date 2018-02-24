234d707e310a4a444835c5f60294d9af69f59a4c
michael bishop <michael.bishop@iohk.io>
2018-02-24 02:49:19 -0400

    [DEVOPS-13] reorganize the nix expressions some default.nix now defines most of the new nix expressions shell.nix is the old nix-shell for impure building linux.nix now obeys XDG_DATA_HOME

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
-with (import (fetchTarball https://github.com/NixOS/nixpkgs/archive/48ecdcf5980a6504cd3b884b121e29efb2fb83dc.tar.gz) {});
-# NOTE: when bumping nixpkgs, also update .travis.yaml
-
-stdenv.mkDerivation {
-  name = "daedalus";
-
-  buildInputs = [electron nodejs-6_x nodePackages.bower nodePackages.node-gyp nodePackages.node-pre-gyp ];
-
-  src = null;
-
-}
+let
+  pkgs = import (import ./fetchNixpkgs.nix {
+    rev = "c831224528cd6bfd49bfc2c18b9c5d9015651077";
+    sha256 = "1idygi6x10wilrmj4w1djby8qi8zphxbp1zkzn4lmwhzfhgx2qnz";
+  }) {
+    config = {};
+    overlays = [];
+  };
+  packages = self: {
+    daedalus = self.callPackage ./linux.nix {};
+    bundle = self.callPackage ./nix-bundle.nix {};
+  };
+in pkgs.lib.makeScope pkgs.newScope packages
